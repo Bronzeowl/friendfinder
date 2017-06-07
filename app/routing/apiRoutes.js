e03c19a313f61e7bfc5e9ce54308584863f2ba23
Marie D <mdagan03@gmail.com>
2017-06-07 07:50:15 -0500

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,69 +1,28 @@
+// DEPENDENCIES
+// ===============================================================================
 var path = require("path");
-var friends = require("../data/friends");
 
-// DISPLAY ALL FRIENDS - provides JSON
-module.exports=(app)=>{
-    app.get("/api/friends", function(request, response) {
-        response.json(friends);
-    });
+// LOAD DATA
+// ===============================================================================
+var friendData = require("../data/friends");
 
-// CREATE NEW FRIEND- takes in JSON input
-    app.post("/api/friends", function(request, response) {
-        var newFriend = request.body;
-    
-        console.log(newFriend);
+// ROUTING
+// ===============================================================================
 
-        var friendsScores = [];
-        var totalScore = [];
+module.exports = function (app) {
+  // API GET Requests
+  // ---------------------------------------------------------------------------
 
-        var newFriendScores = setStringsToNumbers(newFriend.scores);
-        console.log("Coverted N: " + newFriendScores);
+  app.get("/api/friends", function (req, res) {
+    res.json(friendData);
+  });
 
-        for(var i = 0; i < friends.length; i++) {
-            friendsScores[i] = setStringsToNumbers(friends[i].scores);
-            console.log("Coverted " + i + ": " + friendsScores[i])
-            totalScore[i] = (getTotal(friendsScores[i], newFriendScores));
-            console.log("Total " + i + ": " + totalScore[i]);
-        }
+    // API POST Requests
+  // ---------------------------------------------------------------------------
 
-        console.log("Best match: " + friends[getLowestTotalIndex(totalScore)].name);
-        response.json(friends[getLowestTotalIndex(totalScore)]);
+  // app.post("/api/friends", function (req, res) {
 
-        friends.push(newFriend);
-    });
 
-    function setStringsToNumbers(strArray){
-        var numbers = [];
-        
-        for(var i = 0; i < 10; i++)
-            numbers[i] = parseInt(strArray[i]);
-        
-        return numbers;
-    }
-
-    function getTotal(numArray1, numArray2){
-        var difference = [];
-        var total = 0;
-        
-        for(var i = 0; i < 10; i++) {
-            difference[i] = Math.abs(numArray1[i] - numArray2[i]);
-            total += difference[i];   
-        }
-    
-        return total;
-  }
-
-    function getLowestTotalIndex(numArray) {
-        var min = numArray[0];
-        var minIndex = 0;
-
-        for (var i = 1; i < numArray.length; i++) {
-            if (numArray[i] < min) {
-                minIndex = i;
-                min = numArray[i];
-            }
-        }
-
-        return minIndex;
-    }
+  // });
 }
+
